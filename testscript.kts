799b66131ed5d7fc3f35256f4feb0cf76e4085e9
ibonny <ibonny@gmail.com>
2019-12-04 21:26:35 +0000

    Saving changes.

--- testscript.kts
+++ testscript.kts
@@ -5,6 +5,11 @@ import khttp.get
 
 import com.beust.klaxon.Klaxon
 import com.beust.klaxon.JsonObject
+import com.beust.klaxon.JsonArray
+import com.beust.klaxon.PathMatcher
+
+import java.util.regex.Pattern
+
 import java.io.StringReader
 
 data class Todo(
@@ -14,15 +19,48 @@ data class Todo(
     val completed: String
 )
 
-val header = mapOf(
-    "
+//val result = get("http://jsonplaceholder.typicode.com/todos")
 
-val result = get("http://jsonplaceholder.typicode.com/todos")
+//val jsonData = Klaxon().parseJsonArray(StringReader(result.text))
 
-val jsonData = Klaxon().parseJsonArray(StringReader(result.text))
+//for (entry in jsonData) {
+//    val obj = entry as JsonObject
+
+//    println("${obj.int("id")} ${obj.string("title")}")
+//}
+
+val result = get("https://ghibliapi.herokuapp.com/films")
+
+val jsonData = Klaxon()
+    .parseJsonArray(StringReader(result.text))
+    .filter { 
+        val obj = it as JsonObject
+
+        obj.string("title")?.contains("the")!!
+    }
+    // .map {
+    //     val obj = it as JsonObject
+
+    //     obj.string("title")!!
+    // }
+
+// for (entry in jsonData) {
+//     // This cast is required for some reason, as it's not recognized as a JsonObject beforehand.
+//     val obj = entry as JsonObject
+
+//     println("${obj.string("id")} ${obj.string("title")}")
+// }
 
 for (entry in jsonData) {
     val obj = entry as JsonObject
 
-    println("${obj.int("id")} ${obj.string("title")}")
+    println(obj)
+
+    val arr = entry.get("people") as JsonArray<String>
+
+    for (person in arr) {
+        // val obj2 = person as JsonObject
+
+        println(person)
+    }
 }
